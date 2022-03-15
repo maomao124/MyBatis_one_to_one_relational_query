24b8ba4e0f663cbf0eed1a2e25654afdec38a888
毛 <1296193245@qq.com>
2022-03-15 22:17:55 +0800

    MyBatis_one_to_one_relational_query

---
+++ src/main/java/mapper/StudentMapper.java
@@ -0,0 +1,19 @@
+package mapper;
+
+/**
+ * Project name(项目名称)：MyBatis一对一关联查询
+ * Package(包名): mapper
+ * Interface(接口名): StudentMapper
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/3/15
+ * Time(创建时间)： 22:13
+ * Version(版本): 1.0
+ * Description(描述)： 无
+ */
+
+public interface StudentMapper
+{
+    public data.Student getStudentInformation(Long student_no);
+}
